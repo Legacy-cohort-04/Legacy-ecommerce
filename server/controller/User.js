e739212367110994104723da407642a51d14cfd7
FatmaChirchi <chirchifatma@gmail.com>
2024-11-27 16:15:41 +0100

    signin signup backend done

---
+++ server/controller/User.js
@@ -0,0 +1,119 @@
+const db = require("../database/index");
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config
+
+
+
+const validatePassword=(password)=>{
+    const errors=[]
+    const passwordChecking=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[a-zA-Z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+
+    if(password.length < 8){
+        errors.push("Password must contain at least 8 characters.")
+    }
+    if(!passwordChecking.test(password)){
+        errors.push("Password must contain at least one upper case, one lower case, and one symbol")
+    }
+    return {
+        isValid:errors.length===0,
+        errors:errors
+    }
+}
+const signup = async (req, res) => {
+    try {
+        const {
+            email,
+            image,
+            password,
+            name
+        } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password,10);
+      
+
+        console.log(req.body);
+
+        if (!email || !password || !name) {
+            return res.status(400).send('Missing required fields');
+        }
+
+        const passwordValidation = validatePassword(password);
+        if (!passwordValidation.isValid) {
+            return res.status(400).json({
+                message: 'Password is too weak',
+                errors: passwordValidation.errors
+            });
+        }
+
+
+
+        const getuser = await db.User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (getuser) {
+            return res.status(400).send("User already exists");
+        } else {
+            const user = await db.User.create({
+                email: email,
+                image:image,
+                password: hashedPassword,
+                name: name
+            });
+
+            console.log('User created:', user);
+            res.send(user)
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const {
+            email,
+            password,
+        } = req.body;
+
+        const user = await db.User.findOne({ where: { email } })
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, name: user.name, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                type: user.type
+            },
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+};
+
+
+module.exports = { signup, login };
--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const userRoute = require("./routes/User");
 const app = express();
-const products =require("./routes/Products.js")
- const favorite=require("./routes/Favourites.js")
- const cartProducts=require("./routes/CartProducts.js")
 
-app.use(cors());
+
+app.use(cors()); 
 app.use(express.json());
 
 const PORT = 3000;
-app.use("/cartP",cartProducts)
-app.use("/favorite",favorite)
-app.use('/products',products)
-app.use("/api/users", userRoute)
+
 require("./database/index.js")
 
-const adminCreateRoute=require('./routes/Admin.js')
 
-app.use('/admin',adminCreateRoute)
 
 
 
