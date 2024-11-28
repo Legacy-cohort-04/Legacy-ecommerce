3656d7f2ab7162286dfca782c6e4f846eaad1fce
FatmaChirchi <chirchifatma@gmail.com>
2024-11-28 18:30:35 +0100

    login almost done

--- ecommerce/src/pages/Auth/login.tsx
+++ ecommerce/src/pages/Auth/login.tsx
@@ -2,7 +2,10 @@ import { FormEvent } from 'react'
 import { useRouter } from 'next/router'
 import { useState } from 'react';
 import axios, { AxiosError } from 'axios'
-import '../Auth/login.css'
+import styles from './login.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export interface UserLogin {
   email: string,
@@ -13,6 +16,8 @@ export default function Login() {
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
   const [error, setError] = useState<string>("")
+  const [isEmailStep, setIsEmailStep] = useState(true);
+
 
   const navigate = useRouter();
 
@@ -51,66 +56,162 @@ export default function Login() {
     } catch (err: unknown) {
       const axiosError = err as AxiosError
       console.error(err);
-      if (axiosError.response){
+      if (axiosError.response) {
         const errorData = axiosError.response.data as string;
-  
-      if (errorData === "Invalid credentials") {
-        setError("Incorrect password. Please try again.");
-      } else {
-        setError("An error occurred during login. Please try again later.");
+
+        if (errorData === "Invalid credentials") {
+          setError("Incorrect password. Please try again.");
+        } else {
+          setError("An error occurred during login. Please try again later.");
+        }
       }
-    }}
+    }
   }
-    return (
-         <div className="login-container">
-            <form className="login-form" onSubmit={handleLogin}>
-            <h2 className="login-title">Login</h2>
-                <div className="form-group">
-                    <label htmlFor="email" className="form-label">Email Address</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Email Address"
-                        required
-                        className="form-input"
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                        required
-                        className="form-input"
-                    />
-                </div>
-
-                {error && (
-                    <div className="error-message">
-                        <p>{error}</p>
-                    </div>
-                )}
-
-                <button type="submit" className="login-button"  >Login</button>
-
-              
-            <div className="signup-link">
-                    <p>
-                        Don't have an account? <a  className="signup-link-text" onClick={()=>navigate.push("/Auth/signup")}>Sign up</a>
-                    </p>
-                </div>
-
-               
-            </form>
+  return (
+    <div>
+      <div className="login-container">
+        <div className="login-image-container">
+          <img
+            src="https://res.cloudinary.com/dc9siq9ry/image/upload/v1732056264/hnmnjb0lmjvlvzgygkku.webp"
+            alt="Login illustration"
+            className="login-image"
+          />
+          <h4 className="Logingretting" style={{ "marginRight": "160px" }}>Explore the world of meta fashion </h4>
         </div>
 
-    )
-  }
+        <form className="login-form" onSubmit={handleLogin}>
+          <h3 className="loginMessage" style={{ "marginBottom": "0px", "position": "relative", "right": "20px" }}>Login</h3>
+          <div className="login-link">
+            <p>
+              New User?{' '}
+              <a className="login-link-text" style={{ "cursor": "pointer" }} onClick={() => navigate.push("/user/signup")}>
+                Create an account
+              </a>
+            </p>
+          </div>
+
+          {isEmailStep ? (
+            <div>
+              <input
+                type="email"
+                className="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email Address"
+                required
+                autoComplete='off'
+              />
+            </div>
+          ) : (
+            <div className="password-container">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+                autoComplete='off'
+              />
+            </div>
+          )}
+
+          {error && (
+            <div className="error-message">
+              <p>{error}</p>
+            </div>
+          )}
+
+          <button type="submit" className={styles.loginButton}>
+            {isEmailStep ? 'Continue' : 'Login'}
+          </button>
+
+          <div className="or-container">
+            <p style={{ "marginBottom": "-2px" }}>Or</p>
+          </div>
+
+          <div className="social-buttons">
+            <button
+              className="social-button google-button"
+            >
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: '10px', fontSize: '24px' }} /> Continue with Google
+            </button>
+            <button
+              className="social-button facebook-button"
+            >
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: '10px', fontSize: '24px' }} /> Continue with Facebook
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className={styles.loginContainer}>
+        <div className="login-image-container">
+          <img
+            src="https://res.cloudinary.com/dc9siq9ry/image/upload/v1732056264/hnmnjb0lmjvlvzgygkku.webp"
+            alt="Login illustration"
+            className="login-image"
+          />
+          <h4 className="Logingretting" style={{ "marginRight": "160px" }}>Explore the world of meta fashion </h4>
+        </div>
+        <form className={styles.loginForm} onSubmit={handleLogin}>
+          <h2 className={styles.loginTitle}>Login</h2>
+          <div className="login-link">
+            <p>
+              New User?{' '}
+              <a className="login-link-text" style={{ "cursor": "pointer" }} onClick={() => navigate.push("/user/signup")}>
+                Create an account
+              </a>
+            </p>
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email Address"
+              required
+              className={styles.formInput}
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+              className={styles.formInput}
+            />
+          </div>
+
+          {error && (
+            <div className="error-message">
+              <p>{error}</p>
+            </div>
+          )}
+
+          <button type="submit" className={styles.loginButton}  >Login</button>
+          <div className="social-buttons">
+            <button
+              className={styles.socialButton}             >
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: '10px', fontSize: '24px' }} /> Continue with Google
+            </button>
+            <button
+              className={styles.socialButton}
+            >
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: '10px', fontSize: '24px' }} /> Continue with Facebook
+            </button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  )
+}
