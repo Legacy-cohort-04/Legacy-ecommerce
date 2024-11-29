f8bb4ca9e02617c56cc06b6e3783e1ec3bf4e870
FatmaChirchi <chirchifatma@gmail.com>
2024-11-29 15:32:08 +0100

    login sign up done

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const app = express();
 const products =require("./routes/Products.js")
  const cartProducts=require("./routes/Cart.js")
+ const userroute=require("./routes/User.js")
 
 
 app.use(cors()); 
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 app.use("/cartP",cartProducts)
 app.use('/products',products)
 require("./database/index.js")
