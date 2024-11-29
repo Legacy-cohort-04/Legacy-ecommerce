7bd5e4a0b8ddfe566c8266accbacd263325ea0f3
ahmed <ahmedboukottaya01@gmail.com>
2024-11-29 22:00:34 +0100

    pull signup final

---
+++ server/chat.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '../ecommerce/src/pages/chat/chat.html'));
+});
+
+  io.on('connection', (socket) => {
+    io.emit('chat message', "New User Connected");
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+  });
+
+server.listen(4000, () => {
+  console.log('server running at http://localhost:4000');
+});
