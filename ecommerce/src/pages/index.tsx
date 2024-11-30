c8d0d5155795262fa11db3fd9c840f73f9dbd690
Malek-Ad <malloukaad@gmail.com>
2024-11-30 14:05:14 +0100

    working admin

--- ecommerce/src/pages/index.tsx
+++ ecommerce/src/pages/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 
 
+
 const index = () => {
 const navi = useRouter()
   return (
-    <div onClick={()=> navi.push("/Home/home") } >salem</div>
+    <div onClick={()=> navi.push("/Admin/AdminStatistics") } >salem</div>
   )
 }
 
