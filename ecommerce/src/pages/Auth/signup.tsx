65c074c349fbcd6fc334bf1e2983f12d9d936cc1
FatmaChirchi <chirchifatma@gmail.com>
2024-12-01 14:16:28 +0100

    added condition to login as admin

--- ecommerce/src/pages/Auth/signup.tsx
+++ ecommerce/src/pages/Auth/signup.tsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2'
 
+
 export interface User {
   id: number,
   firstName: string,
